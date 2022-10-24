6e3ae2282ac86a4ce99884aa22e961b6ec9dc53a
Pranav Mehta <94271480+DaPhysikist@users.noreply.github.com>
2022-10-24 01:37:45 -0700

    filled in fields for task class

--- src/Task.java
+++ src/Task.java
@@ -1,9 +1,65 @@
-public class Task {
-    String taskName;
-    String appID;
+import java.time.*;
+import java.net.URL;
+import java.io.File;
 
-    public Task(String taskName, String appID){
+public class Task {
+    private String taskName;
+    private String taskSubject;
+    private int taskPoints;
+    private String taskDetails;
+    private LocalDateTime dueTime;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Duration estimatedDuration;
+    //private URL taskLink;
+    //private File taskFile;
+    private String appID;
+
+    //we need a whole bunch of constructors for the many different task creation possibilities
+
+    public Task(String taskName, LocalDateTime dueTime, String appID){
         this.taskName = taskName;
+        this.dueTime = dueTime;
         this.appID = appID;
+    }
+
+    public Task(String taskName, Duration estimatedDuration){
+        this.taskName = taskName;
+        this.estimatedDuration = estimatedDuration;
+    }
+
+    public Task(String taskName, LocalDateTime startTime, LocalDateTime endTime){
+        this.taskName = taskName;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.estimatedDuration = calculateEstimatedDuration(startTime, endTime);
+    }
+
+    public Duration calculateEstimatedDuration(LocalDateTime start, LocalDateTime end){
+        return Duration.between(start, end);
+    }
+
+    public String getTaskName(){
+        return this.taskName;
+    }
+
+    public LocalDateTime getDueTime(){
+        return this.dueTime;
+    }
+
+    public LocalDateTime getStartTime(){
+        return this.startTime;
+    }
+
+    public LocalDateTime getEndTime(){
+        return this.endTime;
+    }
+
+    public Duration getEstimatedDuration(){
+        return estimatedDuration;
+    }
+
+    public String getAppID(){
+        return this.appID;
     }
 }
