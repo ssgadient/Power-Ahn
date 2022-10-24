16d7be914e5d2c8ef196d49a6a011c81857af658
NguyenSanh <jsn006@ucsd.edu>
2022-10-24 04:07:22 -0700

    Added Local Server User Info
    Successfully connected to MySQL server with Java JDBC

--- src/TaskDatabase.java
+++ src/TaskDatabase.java
@@ -1,3 +1,4 @@
+
 // Look into JBDC 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -5,18 +6,18 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class TaskDatabase {
-    static final String DB_URL = "jdbc:mysql://localhost/3306";
-    static final String USER = "guest";
-    static final String PASS = "guest123";
+    static final String DB_URL = "jdbc:mysql://localhost:3306";
+    static final String USER = "root"; // Local server user
+    static final String PASS = "sanh2001"; // Local server password
 
     // public Database {
     // }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // Open a connection
         try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
                 Statement stmt = conn.createStatement();) {
-            String sql = "CREATE DATABASE STUDENTS";
+            String sql = "CREATE DATABASE STUDENTS"; // Database creation
             stmt.executeUpdate(sql);
             System.out.println("Database created successfully...");
         } catch (SQLException e) {
