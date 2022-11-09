7b483894f7ec101d73439247747059c00d6de2a5
Pranav Mehta <94271480+DaPhysikist@users.noreply.github.com>
2022-11-08 20:00:44 -0800

    did timer stuff

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
 import java.util.*;
 
 public class MainFrame extends Application {
-    public static void main(String[] args) {
+    public static void openApp(String[] args) {
         launch(args);
     }
 
