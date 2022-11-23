43ec5750858081d3df54e9ef290fd60ce5a6c800
Pranav Mehta <94271480+DaPhysikist@users.noreply.github.com>
2022-11-22 22:46:20 -0800

    merged with Database

--- app/src/main/java/powered_tasks/testing/CountdownTimer.java
+++ app/src/main/java/powered_tasks/testing/CountdownTimer.java
@@ -4,23 +4,8 @@ import java.util.TimerTask;
 public class CountdownTimer {
     public void countdown(int totalSeconds){
         final Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask(){           @Override
-            public void run(){
-                //TimerUI.updateTimer(totalSeconds);
-                if (totalSeconds < 0){
-                    timer.cancel();
-                }
-            }
-        }, 0, 1000);
-    }
-}
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class CountdownTimer {
-    public void countdown(int totalSeconds){
-        final Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask(){           @Override
+        timer.scheduleAtFixedRate(new TimerTask(){          
+            @Override
             public void run(){
                 //TimerUI.updateTimer(totalSeconds);
                 if (totalSeconds < 0){
